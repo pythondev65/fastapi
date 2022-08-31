6139bb53a63795d155ae0048521a65805860d2d1
pythondev65 <pythondev65@gmail.com>
2022-08-31 14:55:55 +0530

    chnages for fast api

---
+++ main.py
@@ -0,0 +1,45 @@
+
+
+from asyncio import sleep
+import asyncio
+from typing import Union
+from fastapi.responses import JSONResponse
+from fastapi import Body, FastAPI, status
+from fastapi import FastAPI
+import logging
+
+
+app = FastAPI()
+
+
+@app.get("/valid/")
+async def read_item(is_valid_input):
+    print(type(is_valid_input))
+    print("data=>", is_valid_input.isdigit())
+    if is_valid_input.isupper():
+        return {"Fail"}
+    elif is_valid_input.islower():
+        return {"Success"}
+    elif is_valid_input.isdigit():
+           return JSONResponse(status_code=400, content="Integer is not allowed")
+    else:
+        return JSONResponse(status_code=501, content="data type float is not allowed")
+    
+    
+@app.post("/process/")
+async def read_item(x):
+        data=x
+        print(f'asyncio message worker 2 Hello!')
+        await asyncio.sleep(180)
+        query="INSERT INTO dataset(input) VALUES(data)"
+        return {"Success"}
+        
+    
+    
+
+
+            
+           
+
+        
+        
